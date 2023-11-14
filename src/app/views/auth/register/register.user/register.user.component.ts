7cdba008e75d43262468412830b8a52d3cf5ef20
Juan David Herrera Ramírez <jd13hr@gmail.com>
2023-11-13 21:20:33 -0500

    Se realiza el diseño base del componente Register, falta la lógica. Adicional se modifican los caracteres raros del componente Welcome

--- src/app/views/auth/register/register.user/register.user.component.ts
+++ src/app/views/auth/register/register.user/register.user.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './register.user.component.html',
   styleUrls: ['./register.user.component.css']
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit{
+  constructor(private socket: Socket, private router:Router){}
 
+  public login = new FormGroup({
+    nickname : new FormControl('', [Validators.required]),
+    password : new FormControl('', [Validators.required])
+  });
+
+  public login_submit(event:any):void{
+    event.preventDefault();
+    if(this.login.invalid) return
+    let {nickname, password} = this.login.value;
+
+    this.socket.emit('login', {nickname: nickname?.trim().toLowerCase(), password} , (res:any) =>{
+      if(res.successful){
+        localStorage.setItem('identity', JSON.stringify(res.data));
+        this.router.navigate(['home']);
+      }
+      console.log(res)
+    });
+  }
+
+  ngOnInit(): void {
+    const identity = localStorage.getItem('identity');
+    if(identity) this.router.navigate(['home/chat'])
+
+    this.socket.on('login', (res:any) =>{
+      console.log(res)
+      if(res.successful){
+        // this.message = '';
+        // localStorage.setItem('token', res.token);
+        // this.router.navigate(['home']);
+      }else {
+        // this.message = res.message
+      }
+    });
+  }
 }
